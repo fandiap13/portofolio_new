5d21079f221e292c24eb4a922dbec8856b90e53d
Fandi Az <andiazizp123@gmail.com>
2024-10-22 21:55:21 +0700

    update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
     },
     extend: {
+      textShadow: {
+        custom: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+      },
       colors: {
         dark: "#32343a",
         light: "#eeeff1",
